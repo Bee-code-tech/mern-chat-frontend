a1fe80fad1ff04cc4fa2122ac725c37fc5576ee1
bee-code-tech <babawaleolatunji64@gmail.com>
2024-06-05 03:30:50 +0100

    cleanup

--- src/pages/Notification/Notification.jsx
+++ src/pages/Notification/Notification.jsx
@@ -1,5 +1,4 @@
-import React, {useState} from 'react'
-import { useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 import NotificationCard from './NotificationCard'
 import picf from '../../assets/note.png'
 import NotificationSkeleton from '../../components/skeletons/NotificationSkeleton'
