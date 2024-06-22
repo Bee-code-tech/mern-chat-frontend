24d2b40ec67282d1ed2c83ceedced714865c1b28
bee-code-tech <babawaleolatunji64@gmail.com>
2024-06-22 22:29:07 +0100

    font changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        gilroy: ['Gilroy', 'sans-serif'],
+      },
+    },
   },
   plugins: [
     require('daisyui'),
   ],
 };
+
+
