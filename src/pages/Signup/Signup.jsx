cd343070fd3ab28133eb987d4ef28d314d99b8e4
bee-code-tech <babawaleolatunji64@gmail.com>
2024-06-29 06:06:23 +0100

    kkk

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -25,7 +25,7 @@ const Signup = () => {
         await signup(inputs);
     };
     return (
-        <div className="min-h-screen  overflow-y-auto flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="min-h-screen  overflow-y-auto flex items-center justify-center bg-white py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full space-y-8">
                 <div>
                     <h2 className="mt-14 text-center text-3xl font-extrabold text-gray-900">
@@ -64,9 +64,7 @@ const Signup = () => {
                         </button>
                     </div>
                 </form>
-                <p className="mt-2 text-center text-sm text-gray-600">
-                    Already have an account? <Link to="/login" className="font-medium text-green-600 hover:text-green-500">Sign in</Link>
-                </p>
+              
             </div>
         </div>
 
